6c9fde08a621a20b1940d92e440906eca0c4f53f
Min Ah Jung <149572033+Minah-JUNG@users.noreply.github.com>
2024-03-25 09:40:55 +0900

    Delete VariantCaling/4_SNPcalling_bcftools.sh

--- VariantCaling/4_SNPcalling_bcftools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read F; do
-
-	bcftools mpileup --threads 40 -O u -f ./1_Reference/GCF_000001405.40_GRCh38.p14_genomic.fasta ./3_Fixed_bam/$F\_fixed.bam | bcftools call --threads 40 -f GQ -V indels -vmO z > $F\_raw.vcf.gz &
-
-done<./$1
-
-	echo "work"
