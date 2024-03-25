9eaf0f64d1caf2ea69826f5e30615362cee9db91
Min Ah Jung <149572033+Minah-JUNG@users.noreply.github.com>
2024-03-25 09:41:03 +0900

    Delete VariantCaling/5_SNPfiltering_bcftools.sh

--- VariantCaling/5_SNPfiltering_bcftools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read F; do
-
-	bcftools filter -S . -i 'GT="1/1" && %QUAL>=30 && GQ>=30 && DP>=10' $F\_raw.vcf.gz | sed '/\.\/\./d' | gzip -c > $F\_bcf_filtered.vcf.gz &
-
-done<./$1
-
-	echo "running"  > message.txt
